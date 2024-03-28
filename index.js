84dd98878b3eae80bccc0b4fa541f074bf79ecb3
roxannesyombua <roxannesyombua@gmail.com>
2024-03-28 20:48:23 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+
+function mondayWork(insert = "go to the office") {
+    return(`This Monday, I will ${insert}.`)
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(flair ='*'){
+    return function(adjective ="special"){
+return `You are ${flair}${adjective}${flair}!`;
+    };
+}
